d9cf6a313f02ea3379d05255348ae17ef2fcc665
omochanocha <76525239+omochanocha@users.noreply.github.com>
2022-08-28 20:26:10 +0900

    セクション4 22.mapやfilterを使った配列の処理まで

--- src/index.js
+++ src/index.js
@@ -1,10 +1,70 @@
-import "./styles.css";
+/* 
+ 分割代入
+*/
+/* const myProfile = {
+  name: "じぇけえ",
+  age: 28
+}; */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です`;
+// console.log(message2);
+
+// const myProfile = ["じゃけえ", 28];
+// 配列の場合はキーが決まってないので順番で渡ってくる
+// const [name, age] = myProfile;
+// const message3 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message3);
+
+/* 
+スプレッド構文
+*/
+// const arr1 = [1, 2];
+// 配列が展開される
+// console.log(...arr1); //1 2
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(...arr1);
+
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+/*
+配列のコピーや結合
+*/
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+/*
+mapやfilterを使った配列の処理
+*/
+// const nameArr = ["田中", "山田", "じゃけえ"];
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+/* const numArr = [1, 2, 3, 4, 5];
+const newNumArr = numArr.filter((num) => {
+  return num % 2 === 1;
+}); */
+// console.log(newNumArr);
+
+/* const newNameArr = nameArr.map((name) => {
+  if (name === "じゃけえ") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+}); */
+// console.log(newNameArr);
